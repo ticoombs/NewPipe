ticoombs/NewPipe

commit 10b943f37e2a5bb2e0029b857045407eb21a1371

DownloadDialog: Avoid using restricted API for menuitem
Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

app/src/main/java/org/schabi/newpipe/download/DownloadDialog.java
@@ -16,6 +16,7 @@ import android.os.IBinder;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -31,7 +32,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.view.menu.ActionMenuItemView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.collection.SparseArrayCompat;
 import androidx.documentfile.provider.DocumentFile;
@@ -113,7 +113,7 @@ public class DownloadDialog extends DialogFragment
     private StoredDirectoryHelper mainStorageAudio = null;
     private StoredDirectoryHelper mainStorageVideo = null;
     private DownloadManager downloadManager = null;
-    private ActionMenuItemView okButton = null;
+    private MenuItem okButton = null;
     private Context context = null;
     private boolean askForSavePath;
 

commit cd4d0cc232473090773b207fd3ed9edd43ec8961

fix(download): Fix ClassCastException when opening download dialog
Fixes crash caused by incorrect MenuItem retrieval in initToolbar().
The code was using toolbar.findViewById() which returns a View, but
okButton is declared as MenuItem. Changed to use toolbar.getMenu()
.findItem() to properly retrieve the MenuItem.

This bug was introduced in commit 10b943f37 which changed the type
from ActionMenuItemView to MenuItem but didn't update the retrieval
method.

Fixes: java.lang.ClassCastException: ActionMenuItemView cannot be cast to MenuItem

app/src/main/java/org/schabi/newpipe/download/DownloadDialog.java
@@ -344,7 +344,7 @@ public class DownloadDialog extends DialogFragment
         toolbar.setNavigationOnClickListener(v -> dismiss());
         toolbar.setNavigationContentDescription(R.string.cancel);
 
-        okButton = toolbar.findViewById(R.id.okay);
+        okButton = toolbar.getMenu().findItem(R.id.okay);
         okButton.setEnabled(false); // disable until the download service connection is done
 
         toolbar.setOnMenuItemClickListener(item -> {